f5f6381d131fa36c0ae4ec04bf4bc93f6bed7270
Toni Lima <limatoni104@gmail.com>
2023-10-14 21:58:06 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: Toni Lima <limatoni104@gmail.com>

---
+++ src/main/java/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,55 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    // atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    // Adicionar uma nova tareda à lista com descrição fornecida.
+    public void adicionarTarefa(String descricao) {
+       tarefaList.add(new Tarefa(descricao));
+    }
+
+
+    // Remove uma tarefa da lista com base em uma descrição.
+    public  void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    // Retorna uma lista contendo a descrição de todas as taerfas na lista.
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa  listaTarefa = new ListaTarefa();
+        System.out.println(" O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        System.out.println(" O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O número total de lementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+
+        listaTarefa.obterDescricoesTarefas();
+
+    }
+}
